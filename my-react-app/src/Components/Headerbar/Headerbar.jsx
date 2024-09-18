ca35988ff90d7cc13141b53245c053d3b743bd84
VemuAbhinav <93081725+VemuAbhinav@users.noreply.github.com>
2024-09-19 00:05:46 +0530

    finished the sidebar

--- my-react-app/src/Components/Headerbar/Headerbar.jsx
+++ my-react-app/src/Components/Headerbar/Headerbar.jsx
@@ -4,19 +4,38 @@ import './Headerbar.css'
 const Headerbar = () => {
 
   const [menu,setMenu]=useState("Home")
+  const [isSidebarOpen,setIsSidebarOpen]=useState(false)
   return (
     <div>
         <div id="myHeader">
             <h1 className='heading' onClick={()=>{setMenu("Home")}}>AVENGERS</h1>
             <nav>
+               {/* Hamburger Icon */}
+              <div className="hamburger-icon" onClick={()=>{setIsSidebarOpen(!isSidebarOpen)}}>
+                {isSidebarOpen ? (
+                  <span className="close-icon">&times;</span>
+                ) : (
+                  <span className="hamburger">&#9776;</span>
+                )}
+              </div>
                 <ul className='navMenu'>
                     <li onClick={()=>{setMenu("Ironman")}}>About{menu==="Ironman"?<hr/>:<></>}</li>
                     <li onClick={()=>{setMenu("Captain America")}}>News{menu==="Captain America"?<hr/>:<></>}</li>
                     <li onClick={()=>{setMenu("Thor")}}>Comics{menu==="Thor"?<hr/>:<></>}</li>
                     <li onClick={()=>{setMenu("Hulk")}}>Shows{menu==="Hulk"?<hr/>:<></>}</li>
                 </ul>
-                <p className='menu-icon'>@</p>
+                
+                <ul className={`sidebar ${isSidebarOpen? "open":""}`}>
+                    <li onClick={()=>{setMenu("Ironman")}}>About{menu==="Ironman"?<hr/>:<></>}</li>
+                    <li onClick={()=>{setMenu("Captain America")}}>News{menu==="Captain America"?<hr/>:<></>}</li>
+                    <li onClick={()=>{setMenu("Thor")}}>Comics{menu==="Thor"?<hr/>:<></>}</li>
+                    <li onClick={()=>{setMenu("Hulk")}}>Shows{menu==="Hulk"?<hr/>:<></>}</li>
+                </ul>
+                
             </nav>
+        </div>
+        <div>
+
         </div>
         
     </div>
