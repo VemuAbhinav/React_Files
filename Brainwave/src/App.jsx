a19499054e681289d3dc2847fa1760f984cb9a55
VemuAbhinav <93081725+VemuAbhinav@users.noreply.github.com>
2024-10-22 19:00:31 +0530

    changed

--- Brainwave/src/App.jsx
+++ Brainwave/src/App.jsx
@@ -1,10 +1,15 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import "./App.css";
+import Button from './components/Button';
+import Header from './components/Header';
 
 const App = ()=> {
   return (
     <>
-      <h1>Hello world</h1>
+      
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'> 
+        <Header/>
+      </div>
       <ButtonGradient/>
     </>
   );
