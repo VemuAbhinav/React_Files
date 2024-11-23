ce7b7b1df02d596b163719686ee9d610085e4ee9
VemuAbhinav <93081725+VemuAbhinav@users.noreply.github.com>
2024-11-23 20:37:36 +0530

    aa

--- awards/src/App.jsx
+++ awards/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 text-bold'>Welcome to Awards</h1>
+      <h1 className='text-5xl text-orange-500 text-bold font-robert-medium'>Welcome to Awards</h1>
     </main>
   )
 }
