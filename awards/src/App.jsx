d19c68a662ea56dc132abba8824957e98380d775
VemuAbhinav <93081725+VemuAbhinav@users.noreply.github.com>
2024-12-03 18:32:10 +0530

    added button

--- awards/src/App.jsx
+++ awards/src/App.jsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <main>
       <h1 className='text-5xl text-orange-500 text-bold font-robert-medium'>Welcome to Awards</h1>
+      <button className='bg-orange-500 text-white px-4 py-2 rounded-md'>Click me</button>
     </main>
   )
 }
