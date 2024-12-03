851aa15c89127e7172b34e7960f2960830ebf711
VemuAbhinav <93081725+VemuAbhinav@users.noreply.github.com>
2024-12-03 18:37:59 +0530

    updated

--- awards/src/App.jsx
+++ awards/src/App.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const App = () => {
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 text-bold font-robert-medium'>Welcome to Awards</h1>
-      <button className='bg-orange-500 text-white px-4 py-2 rounded-md'>Click me</button>
+      <h1 className='text-6xl text-blue-600 font-bold font-sans'>Welcome to Awards</h1>
+      <button className='bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 rounded-lg shadow-md transition-colors'>Click me</button>
     </main>
   )
 }
